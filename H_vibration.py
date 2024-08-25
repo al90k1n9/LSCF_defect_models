692bbf11af7c6b5430c8d3bab63495463ced79a9
Karthikeyan SARAVANABAVAN <karthikeyan98pdy@protonmail.com>
2024-08-25 12:11:10 +0200

    python file to determine the force constant for H vibrations*2

---
+++ H_vibration.py
@@ -0,0 +1,102 @@
+from lib.dft_energies_0K import *
+import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit
+from matplotlib.ticker import FormatStrFormatter as fsf
+
+acell = 1.4641598078E+01 
+Bohr2Angstrom = 0.5291777
+Ha2eV = 27.2114
+ev2J = 1.60219*10**(-19)
+kB = 1.380649 * 10**(-23) #J/K
+hbar = 1.054571817*10**(-34) #reduced planck's constant in J.s
+T_range = np.arange(700,1400)
+
+O58 = np.asarray([7.4076004134E-01,  2.1591012494E-01,  4.1481175529E-01])
+O87 = np.asarray([8.1539519077E-01,  3.1418832058E-01,  4.1970138000E-01])
+O82 = np.asarray([7.5741977865E-01,  1.9588505177E-01,  9.8492142701E-01])
+O88 = np.asarray([8.2533191524E-01,  2.9543639395E-01,  9.7782933190E-01])
+
+first_bond = O87 - O58
+first_bond_length = np.sqrt(np.matmul(np.transpose(first_bond), first_bond)) * acell * Bohr2Angstrom
+
+second_bond = O88 - O82
+second_bond_length = np.sqrt(np.matmul(np.transpose(second_bond), second_bond)) *acell * Bohr2Angstrom
+
+bond_length = (first_bond_length + second_bond_length)/2
+
+delta_bond_lengths_percent = [0,15, -15,-10, -5, 10,5]
+bond_lengths_angstrom = []
+delta_bond_lengths_angstrom = np.asarray(delta_bond_lengths_percent)/100 * bond_length
+bond_lengths_angstrom = delta_bond_lengths_angstrom + bond_length 
+single_hydrogenation_configs = single_hydrogenation_configs/ev2J_p_mol
+
+#shifting energies
+single_hydrogenation_configs -= single_hydrogenation_configs[0]
+
+def quadratic_fit(x, k):
+    return 1/2 * k * x**2
+
+
+force_constant_k, cov = curve_fit(quadratic_fit, delta_bond_lengths_angstrom, single_hydrogenation_configs, [0])
+
+#k = 31.7 #eV/A^2
+k = force_constant_k*1e20 * ev2J #J/m^2
+
+
+mass_hydrogen = 1.67e-27 #kg
+mass_oxygen = 2.6566962 * 10**-26 #kg
+reduced_mass = (mass_hydrogen*mass_oxygen)/(mass_oxygen + mass_hydrogen)
+
+omega = np.sqrt(2*k/reduced_mass)
+f = omega/(2*np.pi) #frequency charactersitic
+wave_number = f/299792458
+
+
+
+
+def vibrational_correction_term(T):
+    Z= 0
+    beta = 1/(kB*T)
+    for n in range(0,4):
+        Z += np.exp(-hbar * beta*omega * (n+0.5))
+        #F_vib = -kB * T * np.log(Z)
+        #print(n,"\t\t", Z,"\t\t", np.exp(-hbar * beta*omega * (n+0.5)),"\t", F/ev2J)
+
+    F_vib = -kB * T * np.log(Z)*N_avagadro #for the sake of consistency keeping every quantity in Joules per mol units
+    return F_vib
+
+
+
+
+if __name__ == "__main__":
+    print(first_bond_length)
+    print(second_bond_length)
+    print(bond_length, "in A")
+
+    print(force_constant_k, "in eV/A^2")
+    fig1, ax1 = plt.subplots(layout="constrained")
+    ax1.plot(delta_bond_lengths_angstrom, single_hydrogenation_configs, marker="s", ls="")
+
+    fit_xlist = np.linspace(min(delta_bond_lengths_angstrom),max(delta_bond_lengths_angstrom), 1000)
+    
+
+    ax1.plot(fit_xlist, quadratic_fit(fit_xlist, force_constant_k[0]), color = "black", ls="dashed")
+    ax1.set_xlabel("u [A]")
+    ax1.set_ylabel("delta total energy [eV]")
+    #plt.ylim(0,)
+
+    print("\n\nDetermination of vibrational correction term to the chemical potential")
+    print("spring constant k of H2 in J/m^2",k)
+    print("reduced mass of H2 in kg: ", reduced_mass)
+    print("harmonic frequncy w0 in m-1: ",wave_number)
+    #print("F correction to the chemical potential in eV: ", F_vib/ev2J_p_mol)
+    print("verifiying the function")
+    print(vibrational_correction_term(1000))
+    F_vib = vibrational_correction_term(T_range)
+    fig2, ax2 = plt.subplots(layout="constrained")
+    ax2.plot(T_range, F_vib/ev2J_p_mol)
+    ax2.set_xlabel("T[K]")
+    ax2.set_ylabel("F_vib [eV]")
+    ax2.set_xlim(min(T_range),max(T_range))
+    ax2.yaxis.set_major_formatter(fsf('%.4f'))
+    plt.show()

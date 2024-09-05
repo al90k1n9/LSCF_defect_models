13a1f6168dabaaf45cf546ad2ccc6e0b39ca898a
Karthikeyan SARAVANABAVAN <karthikeyan98pdy@protonmail.com>
2024-09-05 14:23:21 +0200

    plot delta G(T)

--- SrO_dry_air_condition_executable.py
+++ SrO_dry_air_condition_executable.py
@@ -14,33 +14,50 @@ p_O2 = x_O2 * P
 
 
 fig,ax = plt.subplots(layout='constrained')
+fig2, ax2 = plt.subplots(layout ="constrained")
 
-ylist = case1(T_range, x, p_O2, P)
+ylist, delta_G_list = case1(T_range, x, p_O2, P)
 plt_element_case1 = ax.plot(T_range, ylist, label ="case 1")
+plt_element_case1_dG = ax2.plot(T_range, np.asarray(delta_G_list)/ev2J_p_mol, label="case 1")
 
-ylist = case2(T_range, x, p_O2, P)
+ylist, delta_G_list = case2(T_range, x, p_O2, P)
 plt_element_case2, = ax.plot(T_range, ylist, label = "case 2")
+plt_element_case2_dG = ax2.plot(T_range, np.asarray(delta_G_list)/ev2J_p_mol, label="case 2")
 
-ylist = case3(T_range, x=0.4)
+ylist, delta_G_list = case3(T_range, x=0.4)
 plt_element_case3, =ax.plot(T_range, ylist, label ="case 3")
+plt_element_case3_dG = ax2.plot(T_range, np.asarray(delta_G_list)/ev2J_p_mol, label="case 3")
 
-ylist = case4(T_range, x = 0.4)
+ylist, delta_G_list = case4(T_range, x = 0.4)
 plt_element_case4, = ax.plot(T_range, ylist, label ="case 4")
+plt_element_case4_dG = ax2.plot(T_range, np.asarray(delta_G_list)/ev2J_p_mol, label="case 4")
 
-ylist = case5(T_range, x, p_O2, P)
+ylist, delta_G_list = case5(T_range, x, p_O2, P)
 plt_element_case5 = ax.plot(T_range, ylist, label ="case 5")
+plt_element_case5_dG = ax2.plot(T_range, np.asarray(delta_G_list)/ev2J_p_mol, label="case 5")
 
-ylist = case6(T_range, x=0.4)
+ylist, delta_G_list = case6(T_range, x=0.4)
 plt_element_case6 = ax.plot(T_range, ylist, label ="case 6")
+plt_element_case6_dG = ax2.plot(T_range, np.asarray(delta_G_list)/ev2J_p_mol, label="case 6")
 
 ax.set_title("Dry air conditions")
 ax.set_xlabel("T[K]")
 ax.set_ylabel("[V\'\'\'$_{La}$]")
 
 
+ax2.set_title("Dry air conditions")
+ax2.set_xlabel("T[K]")
+ax2.set_ylabel("${\Delta}G$[eV]")
+
+
 ax.set_xlim(left=T_lower_bound,right=T_upper_bound)
 ax.set_ylim(0,)
 ax.legend(loc="upper left")
+
+ax2.set_xlim(left=T_lower_bound,right=T_upper_bound)
+ax2.set_ylim(0,)
+ax2.legend(loc="upper left")
+
 
 bohr2m = 5.29177e-11
 a_LSCF = 1.46415980775980e+01 * bohr2m
--- lib/SrO_dry_air_models.py
+++ lib/SrO_dry_air_models.py
@@ -6,10 +6,12 @@ from lib.auxilliary_functions import *
 
 def case1(T_range, x=0.4, p_O2 = 0.21, P=1):
     V_Sr= []
+    delta_G_list = []
     delta_E = (E_LSCF_slab_Sr_vac_surf + 2 * E_SrO - (E_LSCF_slab + E_DFT_O2))/2
     for T in T_range:
         mu_O2 = cp_O2(T, E_DFT_O2)
         delta_G = (E_LSCF_slab_Sr_vac_surf + 2 * E_SrO - (E_LSCF_slab + mu_O2))/2
+        if T==963: print("case 1", delta_G/ev2J_p_mol)
         K = np.exp(-delta_G/(R*T))
         #print(K)
 
@@ -24,17 +26,20 @@ def case1(T_range, x=0.4, p_O2 = 0.21, P=1):
         #print(x0_minus- x0_plus)
         #print(equation(x0_minus), equation(x0_plus))
         V_Sr.append(solution[0])
+        delta_G_list.append(delta_G)
         #print(solution[0])
-    return V_Sr
+    return (V_Sr,delta_G_list)
 
 def case2(T_range, x=0.4, p_O2 = 0.21, P=1):
+    delta_G_list =[]
     V_Sr= []
     delta_E = E_LSCF_slab_Sr_vac_bulk + E_SrO - (E_LSCF_slab + 0.5*E_DFT_O2)
     for T in T_range:
         mu_O2 = cp_O2(T, E_DFT_O2)
 
         delta_G = E_LSCF_slab_Sr_vac_bulk + E_SrO - (E_LSCF_slab + 0.5*mu_O2)
-
+        if T==963: print("case 2", delta_G/ev2J_p_mol)
+        delta_G_list.append(delta_G)
         K = np.exp(-delta_G/(R*T))
         #print(K)
 
@@ -47,13 +52,16 @@ def case2(T_range, x=0.4, p_O2 = 0.21, P=1):
         d = -N * x * (1-x)**2
         solution= cubic_model(a,b,c,d)
         V_Sr.append(solution[0])
-    return V_Sr
+    return (V_Sr,delta_G_list)
 
 def case3(T_range, x=0.4):
+    delta_G_list = []
     V_Sr=[]
     delta_E = E_LSCF_slab_Sr_surf_O_sub_surf /2 +  E_SrO - (E_LSCF_slab/2)
     for T in T_range:
         delta_G = delta_E
+        if T==963: print("case 3", delta_G/ev2J_p_mol)
+        delta_G_list.append(delta_G)
 
         K = np.exp(-delta_G/(R*T))
         a = (1-(1/K))
@@ -61,13 +69,16 @@ def case3(T_range, x=0.4):
         c = 3*x
 
         V_Sr.append(quadratic_model(a,b,c,x))
-    return V_Sr
+    return (V_Sr,delta_G_list)
 
 def case4(T_range, x=0.4):
+    delta_G_list = []
     V_Sr=[]
     delta_E = E_LSCF_slab_SrO_bulk +  E_SrO - (E_LSCF_slab)
     for T in T_range:
         delta_G = delta_E
+        if T==963: print("case 4", delta_G/ev2J_p_mol)
+        delta_G_list.append(delta_G)
 
         K = np.exp(-delta_G/(R*T))
         a = (1-(1/K))
@@ -75,14 +86,17 @@ def case4(T_range, x=0.4):
         c = 3*x
 
         V_Sr.append(quadratic_model(a,b,c,x))
-    return V_Sr
+    return (V_Sr,delta_G_list)
 
 def case5(T_range, x=0.4, p_O2 = 0.21, P=1):
+    delta_G_list = []
     V_Sr= []
     delta_E = E_LSCF_bulk_Sr_vac + E_SrO - (E_LSCF_bulk + 0.5*E_DFT_O2)
     for T in T_range:
         mu_O2 = cp_O2(T, E_DFT_O2)
         delta_G = E_LSCF_bulk_Sr_vac + E_SrO - (E_LSCF_bulk + 0.5*mu_O2)
+        if T==963: print("case 5", delta_G/ev2J_p_mol)
+        delta_G_list.append(delta_G)
 
         K = np.exp(-delta_G/(R*T))
         #print(K)
@@ -96,17 +110,20 @@ def case5(T_range, x=0.4, p_O2 = 0.21, P=1):
         d = -N * x * (1-x)**2
         solution = cubic_model(a,b,c,d)
         V_Sr.append(solution[0])
-    return V_Sr
+    return (V_Sr,delta_G_list)
 
 def case6(T_range, x= 0.4):
+    delta_G_list = []
     V_Sr=[]
     delta_E = E_LSCF_bulk_SrO_vac +  E_SrO - (E_LSCF_bulk)
     for T in T_range:
         delta_G = delta_E
+        if T==963: print("case 6", delta_G/ev2J_p_mol)
+        delta_G_list.append(delta_G)
 
         K = np.exp(-delta_G/(R*T))
         a = (1-(1/K))
         b = -3-x
         c = 3*x
         V_Sr.append(quadratic_model(a,b,c,x))
-    return V_Sr
+    return (V_Sr,delta_G_list)

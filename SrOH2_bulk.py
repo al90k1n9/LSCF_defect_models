9fb1e499ac0703139b301455a5d02b09f21d7481
Karthikeyan SARAVANABAVAN <karthikeyan98pdy@protonmail.com>
2025-03-06 11:22:22 +0100

    modified to graph reference G

--- SrOH2_bulk.py
+++ SrOH2_bulk.py
@@ -1,5 +1,6 @@
 from lib.SrOH2_models import * 
 import matplotlib.pyplot as plt
+from matplotlib.ticker import AutoMinorLocator
 
 x = 0.4 #molar fraction of Sr
 x_O2 = 0.21
@@ -13,27 +14,29 @@ p_O2 = x_O2 * P
 p_H2O = x_H2O * P
 
 fig, ax = plt.subplots(layout="constrained")
+fig2, ax2 = plt.subplots(layout="constrained")
 
-V_Sr = case1(T_range)
+V_Sr, delta_G= case1(T_range)
 ax.plot(T_range, V_Sr, label="case1")
+ax2.plot(T_range, np.asarray(delta_G)/ev2J_p_mol, label="case1")
 
-V_Sr = case2(T_range)
+V_Sr, delta_G = case2(T_range)
 ax.plot(T_range, V_Sr, label="case2")
+ax2.plot(T_range, np.asarray(delta_G)/ev2J_p_mol, label="case2")
 
-V_Sr = case3(T_range)
+V_Sr, delta_G = case3(T_range)
 ax.plot(T_range, V_Sr, label="case3")
+ax2.plot(T_range, np.asarray(delta_G)/ev2J_p_mol, label="case3")
 
-V_Sr = case4(T_range)
+V_Sr, delta_G = case4(T_range)
 ax.plot(T_range, V_Sr, label="case4")
+ax2.plot(T_range, np.asarray(delta_G)/ev2J_p_mol, label="case4")
 
 
-V_Sr = case5(T_range)
+V_Sr, delta_G = case5(T_range)
 ax.plot(T_range, V_Sr, label="case5")
+ax2.plot(T_range, np.asarray(delta_G)/ev2J_p_mol, label="case5")
 
-#print(case2(1000))
-#print(case3(1000))
-#print(case4(1000))
-#print(case5(1000))
 ax.set_title("Sr(OH)$_2$ formation")
 ax.set_xlabel("T[K]")
 ax.set_ylabel("[V\'\'\'$_{La}$]")
@@ -43,6 +46,20 @@ ax.set_xlim(left=T_lower_bound,right=T_upper_bound)
 ax.set_ylim(0,)
 ax.legend(loc="upper left")
 
+ax.xaxis.set_minor_locator(AutoMinorLocator())
+ax.yaxis.set_minor_locator(AutoMinorLocator())
 
-ax.legend()
+ax2.set_title("Sr(OH)$_2$ formation")
+ax2.set_xlabel("T[K]")
+ax2.set_ylabel("$\Delta$G$^*$ [eV]")
+
+
+ax2.set_xlim(left=T_lower_bound,right=T_upper_bound)
+#ax2.set_ylim(0,)
+ax2.legend(loc="upper left")
+ax2.xaxis.set_minor_locator(AutoMinorLocator())
+ax2.yaxis.set_minor_locator(AutoMinorLocator())
+
+
+ax2.legend()
 plt.show()
--- lib/SrOH2_models.py
+++ lib/SrOH2_models.py
@@ -13,11 +13,13 @@ def case1(T_range, x=0.4,  p_O2 = 0.21, p_H2O = 0.08, P=1):
     delta_E = ((E_LSCF_slab_Sr_vac_surf + 2 * E_DFT_H2 + E_SrOH2_bulk*2) - (E_LSCF_slab +  4*E_DFT_H2O))/2
     print(delta_E/ev2J_p_mol)
     V_Sr = []
+    delta_G_list=[]
     for T in T_range:
         mu_H2O = cp_H2O(T, E_DFT_H2O)
         mu_H2 = cp_H2(T, E_DFT_H2)
         p_H2 = pH2_giver(T, p_H2O, p_O2)
         delta_G = ((E_LSCF_slab_Sr_vac_surf + 2 * mu_H2 + E_SrOH2_bulk*2) - (E_LSCF_slab +  4*mu_H2O))/2
+        delta_G_list.append(delta_G)
 
         K = np.exp(-delta_G/(R*T))
         N = K * (p_H2O/P)**2 * (P/p_H2)
@@ -28,7 +30,7 @@ def case1(T_range, x=0.4,  p_O2 = 0.21, p_H2O = 0.08, P=1):
         d = -N * x * (1-x)**2
         solution= cubic_model(a,b,c,d)
         V_Sr.append(solution[0])
-    return V_Sr
+    return (V_Sr, delta_G_list)
 
 def case2(T_range, x=0.4, p_O2 = 0.21, p_H2O = 0.08, P=1):
     #uses half oxygen gas and one water moelcule to form hydroxide
@@ -36,11 +38,13 @@ def case2(T_range, x=0.4, p_O2 = 0.21, p_H2O = 0.08, P=1):
     delta_E = ((E_LSCF_slab_Sr_vac_surf + E_SrOH2_bulk *2 ) - (2*E_DFT_H2O + E_DFT_O2 + E_LSCF_slab))/2
     print(delta_E/ev2J_p_mol)
     V_Sr = []
+    delta_G_list = []
     for T in T_range:
         mu_H2O = cp_H2O(T, E_DFT_H2O)
         mu_O2 = cp_O2(T, E_DFT_O2)
 
         delta_G = ((E_LSCF_slab_Sr_vac_surf + E_SrOH2_bulk *2 ) - (2*mu_H2O + mu_O2 + E_LSCF_slab))/2
+        delta_G_list.append(delta_G)
         K = np.exp(-delta_G/(R*T))
         N = K * p_H2O/P * np.sqrt(p_O2/P)
 
@@ -50,31 +54,35 @@ def case2(T_range, x=0.4, p_O2 = 0.21, p_H2O = 0.08, P=1):
         d = -N * x * (1-x)**2
         solution= cubic_model(a,b,c,d)
         V_Sr.append(solution[0])
-    return V_Sr
+    return (V_Sr, delta_G_list)
 
 def case3(T_range, x=0.4, p_O2 = 0.21, p_H2O = 0.08, P=1):
     #case 1 but with both hydrogen atoms stabilised in the sr vacancy
     delta_E = ((E_LSCF_double_hydrogenated + 2* E_SrOH2_bulk) - (E_LSCF_slab + 4*E_DFT_H2O))/2
     print(delta_E/ev2J_p_mol)
     V_Sr = []
+    delta_G_list=[]
     for T in T_range:
         mu_H2O = cp_H2O(T, E_DFT_H2O)
         delta_G = ((E_LSCF_double_hydrogenated + 2* E_SrOH2_bulk) - (E_LSCF_slab + 4*mu_H2O))/2
+        delta_G_list.append(delta_G)
         K = np.exp(-delta_G/(R*T))
         N = (p_H2O/P)**2 * K
         V_Sr.append(N/(1+N) * x)
-    return V_Sr
+    return (V_Sr, delta_G_list)
 
 def case4(T_range, x=0.4, p_O2 = 0.21, p_H2O = 0.08, P=1):
     #one hydrogen stabilised in vacancy and the other as hydrogen gas
     delta_E = ((E_LSCF_single_hydrogenated + 2* E_SrOH2_bulk + E_DFT_H2) - (E_LSCF_slab + 4*E_DFT_H2O))/2
     print(delta_E/ev2J_p_mol)
     V_Sr = []
+    delta_G_list=[]
     for T in T_range:
         mu_H2 = cp_H2(T,E_DFT_H2)
         mu_H2O = cp_H2O(T, E_DFT_H2O)
         p_H2 = pH2_giver(T, p_H2O, p_O2)
         delta_G = ((E_LSCF_single_hydrogenated + 2* E_SrOH2_bulk + mu_H2) - (E_LSCF_slab + 4*mu_H2O))/2
+        delta_G_list.append(delta_G)
         K = np.exp(-delta_G/(R*T))
         N = (p_H2O/P)**2 * (P/p_H2)**(1/2) * K
 
@@ -82,17 +90,19 @@ def case4(T_range, x=0.4, p_O2 = 0.21, p_H2O = 0.08, P=1):
         b = x+N
         c = -N*(x-x**2)
         V_Sr.append(quadratic_model(a,b,c))
+    return (V_Sr, delta_G_list)
 
-    return V_Sr
 
 def case5(T_range, x=0.4, p_O2 = 0.21, p_H2O = 0.08, P=1):
     #one water molecule and one oxygen from the material to form SrOH2
     delta_E = ((E_LSCF_slab_Sr_surf_O_sub_surf + 2*E_SrOH2_bulk) - (E_LSCF_slab + 2*E_DFT_H2O))/2
     print(delta_E/ev2J_p_mol)
     V_Sr = []
+    delta_G_list=[]
     for T in T_range:
         mu_H2O = cp_H2O(T, E_DFT_H2O)
         delta_G = ((E_LSCF_slab_Sr_surf_O_sub_surf + 2*E_SrOH2_bulk) - (E_LSCF_slab + 2*mu_H2O))/2
+        delta_G_list.append(delta_G)
         K = np.exp(-delta_G/(R*T))
         N = K * p_H2O/P
         a = (1-(1/N))
@@ -100,5 +110,4 @@ def case5(T_range, x=0.4, p_O2 = 0.21, p_H2O = 0.08, P=1):
         c = 3*x
 
         V_Sr.append(quadratic_model(a,b,c,x))
-
-    return V_Sr
+    return (V_Sr, delta_G_list)

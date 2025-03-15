748947c0f58e1daf1d671f594c5bfc2e536501a0
Karthikeyan SARAVANABAVAN <karthikeyan98pdy@protonmail.com>
2025-03-15 18:14:02 +0100

    added a plot for delta G

--- SrO_hydroxylated_cond_executable.py
+++ SrO_hydroxylated_cond_executable.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+from matplotlib.ticker import AutoMinorLocator
 from lib.SrO_hydroxylated_models import *
 
 x = 0.4 #molar fraction of Sr
@@ -6,7 +7,7 @@ x_O2 = 0.21
 x_H2O = 0.08
 P = 1 #atm
 T_lower_bound = 700
-T_upper_bound = 1400
+T_upper_bound = 1299
 T_range = np.arange(T_lower_bound,T_upper_bound,1) #K
 #numpy imported chemical potentials, which is imported in humid models
 
@@ -15,20 +16,24 @@ p_H2O = x_H2O * P
 
 
 fig,ax = plt.subplots(layout='constrained')
+fig2, ax2 = plt.subplots(layout="constrained")
 
-V_Sr = case1(T_range, x, p_O2, p_H2O, P)
+V_Sr, delta_G = case1(T_range, x, p_O2, p_H2O, P)
 ax.plot(T_range, V_Sr, label="case 1")
+ax2.plot(T_range, delta_G/ev2J_p_mol, label="case1")
 
-V_Sr = case2(T_range, x, p_H2O, P)
+V_Sr, delta_G = case2(T_range, x, p_H2O, P)
 ax.plot(T_range, V_Sr, label="case 2")
+ax2.plot(T_range, delta_G/ev2J_p_mol, label="case2")
 
-V_Sr = case3(T_range, x, p_O2, p_H2O, P)
+V_Sr, delta_G = case3(T_range, x, p_O2, p_H2O, P)
 ax.plot(T_range, V_Sr, label="case 3")
+ax2.plot(T_range, delta_G/ev2J_p_mol, label="case3")
 
 
 #===================================================
 #PLOTTING OPTIONS
-ax.set_title("Humid conditions")
+ax.set_title("Hydroxilation")
 ax.set_xlabel("T[K]")
 ax.set_ylabel("[V\'\'\'$_{La}$]")
 
@@ -53,5 +58,18 @@ def yaxinvert(x):
 secyax = ax.secondary_yaxis("right", functions=(yaxconvert, yaxinvert))
 secyax.set_ylabel("blocked active surface area / total active surface area")
 
+ax2.set_title("Hydroxilation")
+ax2.set_xlabel("T[K]")
+ax2.set_ylabel("$\Delta$G* [eV]")
+
+ax.xaxis.set_minor_locator(AutoMinorLocator())
+ax.yaxis.set_minor_locator(AutoMinorLocator())
+ax2.xaxis.set_minor_locator(AutoMinorLocator())
+ax2.yaxis.set_minor_locator(AutoMinorLocator())
+
+
+ax2.set_xlim(left=T_lower_bound,right=T_upper_bound)
+#ax2.set_ylim(0,)
+ax2.legend(loc="upper right")
 
 plt.show()

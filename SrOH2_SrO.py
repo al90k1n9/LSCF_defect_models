6c70582c6753cb4120b1eb3580ad14461e50b15b
Karthikeyan <karthikeyan98pdy@pm.me>
2025-04-23 20:41:50 +0200

    new model to form SrO from SrOH2 volatile. some minor changes in graphing in sroh2 volatile formation

---
+++ SrOH2_SrO.py
@@ -0,0 +1,41 @@
+from lib.SrOH2_volatile_models import *
+import matplotlib.pyplot as plt
+from matplotlib.ticker import AutoMinorLocator
+from lib.chemical_potentials import *
+
+
+E_SrOH2 = cp_SrOH2(1)
+print("E SrOH2 in eV: ", E_SrOH2/ev2J_p_mol)
+
+delta_E = E_SrO + E_DFT_H2O - E_SrOH2
+
+print("delta_E in eV", delta_E/ev2J_p_mol)
+
+T_lower_bound = 700 #K
+T_upper_bound = 1200 #K
+
+T_range = np.arange(T_lower_bound, T_upper_bound)
+delta_G_list = []
+
+for T in T_range:
+    mu_H2O = cp_H2O(T, E_DFT_H2O) + zpe_H2O
+    delta_G = E_SrO + mu_H2O - cp_SrOH2(T)
+    delta_G_list.append(delta_G)
+
+fig, ax = plt.subplots(layout="constrained")
+
+delta_G_list = np.asarray(delta_G_list)
+ax.plot(T_range, delta_G_list/ev2J_p_mol)
+
+
+ax.xaxis.set_minor_locator(AutoMinorLocator())
+ax.yaxis.set_minor_locator(AutoMinorLocator())
+
+
+ax.set_xlim(T_lower_bound, T_upper_bound+1)
+
+ax.set_xlabel("T [K]")
+ax.set_ylabel("$\Delta_rG^*(T,p)$")
+
+
+plt.show()
--- SrOH2_volatile_executable.py
+++ SrOH2_volatile_executable.py
@@ -12,7 +12,6 @@ P = 1 #atm
 
 data = np.genfromtxt("./lib/sroh2_factsage.csv", delimiter=";")
 T_range = data[100::100,0]
-print(np.shape(T_range))
 V_Sr, delta_G= case1(T_range)
 
 fig, ax = plt.subplots(layout="constrained")
@@ -20,18 +19,27 @@ fig, ax = plt.subplots(layout="constrained")
 ax.plot(T_range, V_Sr)
 
 ax.set_xlabel("T [K]")
-ax.set_ylabel("[V$_{Sr}$\'\'\']")
+ax.set_ylabel("$[V_{La}\'\'\']_{eq} = x_{eq}$")
 ax.set_xlim(T_range[0], T_range[-1])
 ax.xaxis.set_minor_locator(AutoMinorLocator())
 ax.yaxis.set_minor_locator(AutoMinorLocator())
 
+def yaxconvert(x):
+    return x * 100/0.4
+
+def yaxinvert(x):
+    return x *0.4/100
+
+secyax = ax.secondary_yaxis("right", functions=(yaxconvert, yaxinvert))
+secyax.set_ylabel("% of initial Sr content $\\frac{100 \cdot x_{eq}}{x_0}$")
+secyax.yaxis.set_minor_locator(AutoMinorLocator())
 
 
 fig5, ax5 = plt.subplots(layout="constrained")
 ax5.plot(T_range, delta_G/ev2J_p_mol)
 
 ax5.set_xlabel("T [K]")
-ax5.set_ylabel("$\Delta$G$^*$ [eV]")
+ax5.set_ylabel("$\Delta_r$G$^*(T,p)$ [eV]")
 
 ax5.set_xlim(T_range[0], T_range[-1])
 

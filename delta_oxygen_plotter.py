49c7dfac6a8331ff55b1bfcd23d2a2d45bded776
Karthikeyan <karthikeyan98pdy@pm.me>
2025-06-13 08:06:37 +0200

    graphing modifs

--- delta_oxygen_plotter.py
+++ delta_oxygen_plotter.py
@@ -2,5 +2,9 @@ from lib.auxilliary_functions import *
 import matplotlib.pyplot as plt
 import numpy as np
 
-params = delta_oxygen_interpolater(plot=0)
-print(params)
+params = delta_oxygen_interpolater(plot=1)
+
+
+print("T=973 and the delta is")
+print(params[0]*973 + params[1])
+
